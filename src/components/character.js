68f8bab4acd45752a252e09e373213b37c9d8bc4
Abdul Rahman <ar5438376@gmail.com>
2021-06-04 10:10:58 +0530

    added loader

--- src/components/character.js
+++ src/components/character.js
@@ -1,13 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './character.scss';
 import PropTypes from 'prop-types';
+import Loader from "react-loader-spinner";
 
-const Character = ( {characterData} ) => (
-    <div className="character" >
-        <img src={characterData.image} alt="" className="character__image" />
-        <h3 className="character__name" >{characterData.name}</h3>
-    </div>
-)
+const Character = ({ characterData }) => {
+    const [isImageLoaded, setIsImageLoaded] = useState(false)
+
+    return (
+        <div className="character" >
+            <img src={characterData.image} alt="" className="character__image" onLoad={() => setIsImageLoaded(true)} loading="lazy" />
+            {!isImageLoaded && <div className="character__loader"><Loader type='Oval' color="black" width={60} height={60} /></div>}
+            <h3 className="character__name" >{characterData.name}</h3>
+        </div>
+    )
+}
 
 Character.propTypes = {
     characterData: PropTypes.object
